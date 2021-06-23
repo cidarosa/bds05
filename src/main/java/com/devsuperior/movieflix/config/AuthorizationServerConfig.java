55f04f12efea159e58cedc3a8aef855aa8b777bf
Cida Castello <cidacastello.cursos@gmail.com>
2021-06-23 19:24:09 -0300

    refresh token

--- src/main/java/com/devsuperior/movieflix/config/AuthorizationServerConfig.java
+++ src/main/java/com/devsuperior/movieflix/config/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -49,6 +50,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Autowired
 	private JwtTokenEnhancer tokenEnhancer; // para atualiar os endpoints no método
+	
+	@Autowired
+	private UserDetailsService userDetailsService; //acrescentado
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -59,8 +63,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient(clientId)
-				.secret(passwordEncoder.encode(clientSecret)).scopes("read", "write").authorizedGrantTypes("password")
-				.accessTokenValiditySeconds(jwtDuration);
+				.secret(passwordEncoder.encode(clientSecret))
+				.scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(jwtDuration)
+				.refreshTokenValiditySeconds(jwtDuration);
 	}
 
 	@Override
@@ -69,8 +76,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		TokenEnhancerChain chain = new TokenEnhancerChain();
 		chain.setTokenEnhancers(Arrays.asList(accesTokenConverter, tokenEnhancer));
 
-		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore)
-				.accessTokenConverter(accesTokenConverter).tokenEnhancer(chain);
+		endpoints.authenticationManager(authenticationManager)
+		.tokenStore(tokenStore)
+		.accessTokenConverter(accesTokenConverter)
+		.tokenEnhancer(chain)
+		.userDetailsService(userDetailsService); //acrescentado
 	}
 
 }
